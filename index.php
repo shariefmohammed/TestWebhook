316e12d5d33e82550b09170127bd0a25895e4a92
shariefmohammed <30563498+shariefmohammed@users.noreply.github.com>
2017-07-30 00:29:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Test PHP Version 3 from sharief Test webhook .. ';
+echo 'Test PHP Version 3 from sharief Test webhook .. doest it work ? ';
 
 // Show all information, defaults to INFO_ALL
 phpinfo();
