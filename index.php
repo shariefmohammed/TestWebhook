ca005498a10abce1fb5f69ec4a99399a2c364e8a
shariefmohammed <30563498+shariefmohammed@users.noreply.github.com>
2017-07-30 11:24:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Test PHP Version 3 from sharief Test webhook .. doest it work ? SSL disabled ';
+echo 'Test PHP Version 3 from sharief Test webhook .. doest it work ? SSL disabled 07302017';
 
 // Show all information, defaults to INFO_ALL
 phpinfo();
